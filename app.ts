8be17c7f50db70310dfcd492b60e03e4cc461f83
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-04-20 16:31:51 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,32 @@
+//#! /user/bin/env/node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log("=".repeat(60));
+
+console.log(chalk.bold .blueBright('\n\t\tcode-with-manahil - word counter'));
+const answers: {
+   sentence: string
+ } = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count words :",
+    }
+])
+    
+
+ const words  =  answers.sentence.trim().split(" ")
+console.log (words);
+console.log((chalk.bgMagenta`your sentence words count is ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
+
+
+
+
+
+
+      
+
+
+
+
